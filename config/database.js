1f7ab0c0fc4fe6b8d89964f75626d0686e830ad6
pcerezo <pcerezo70@gmail.com>
2024-12-05 15:55:04 +0100

    Importado dotenv en la configuración de la base de datos

--- config/database.js
+++ config/database.js
@@ -1,23 +1,12 @@
 // config/database.js
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 // Crea una instancia de Sequelize
-/*
+
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   // Cambia si es un servidor remoto
   host: process.env.DB_HOST,
-  port: '3306',
-  dialect: 'mysql',
-  dialectOptions: {
-    charset: 'utf8mb4',
-  },
-  logging: true,            // Desactiva el logging de las consultas SQL en consola
-});
-*/
-
-const sequelize = new Sequelize("lbb7y0hcoqdbe2wm", "oykr0yr7noc59yr8", "mpdr0pn5fwcwynnb", {
-  // Cambia si es un servidor remoto
-  host: "ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: '3306',
   dialect: 'mysql',
   dialectOptions: {
