7f7e93e66ef4edd0bf3a14ca5f95f4b2b24fa56c
pcerezo <pcerezo@users.noreply.github.com>
2024-12-05 14:26:06 +0000

    deploy: 61b6f95962e62ad5e917eadb9ceb9f6e2a55080a

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@
 const { Sequelize } = require('sequelize');
 
 // Crea una instancia de Sequelize
-const sequelize = new Sequelize('chacobache_dev', 'pablo', 'password', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
 //  host: '127.0.0.1',         // Cambia si es un servidor remoto
-  host: 'sqlnet',
+  host: process.env.DB_HOST,
   port: '3306',
   dialect: 'mysql',
   dialectOptions: {
