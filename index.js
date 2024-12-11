03d7c61a40e595106260692f9678f58d853529cd
pcerezo <pcerezo70@gmail.com>
2024-12-11 18:12:46 +0100

    Corregido CORS para aceptar sólo del frontend.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: '*', // Tu frontend en GitHub Pages
+  origin: 'https://pcerezo.github.io', // Tu frontend en GitHub Pages
 }));
 app.use(express.json());
 
